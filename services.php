f8b418a18955ee6dde4ead94755ea228033ee9c6
brownguo <yexuan.guo@codedance.cn>
2019-03-08 15:34:01 +0800

    timer demo

--- services.php
+++ services.php
@@ -12,29 +12,33 @@ require_once __DIR__ . '/Autoloader.php';
 
 class Mail
 {
-    // 注意，回调函数属性必须是public
+    private static $time_interval = 0.5;
+    private static $timer_id;
+
     public function send($to, $content)
     {
-        echo "send mail".PHP_EOL;
+        echo "send mail".date('Y-m-d H:i:s',time()).PHP_EOL;
     }
 
     public function sendLater($to, $content)
     {
-        // 回调的方法属于当前的类，则回调数组第一个元素为$this
-        Timer::add(1, array($this, 'send'), array($to, $content), false);
+        self::$timer_id = Timer::add(self::$time_interval, array($this, 'send'), array($to, $content), true);
     }
 }
 
 $task = new Worker();
-$task->count = 5;
+$task->count = 10;
 $task->onWorkerStart = function($task)
 {
-    // 10秒后发送一次邮件
-    $mail = new Mail();
-    $to = 'workerman@workerman.net';
-    $content = 'hello workerman';
-    $mail->sendLater($to, $content);
-};
 
+    if($task->id === 0)
+    {
+        $mail = new Mail();
+        $to = 'workerman@workerman.net';
+        $content = 'hello workerman';
+        $mail->sendLater($to, $content);
+    }
+
+};
 // 运行worker
 Worker::runAll();
