d9fa5e22ba20aa6377d4e1329ede9d60462e290b
brownguo <yexuan.guo@codedance.cn>
2019-03-12 11:16:52 +0800

    logger

---
+++ Lib/logger.php
@@ -0,0 +1,165 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: guoyexuan
+ * Date: 2018/11/29
+ * Time: 5:05 PM
+ */
+
+namespace Workerman\Lib;
+
+class logger
+{
+    public static $log_file_path = __DIR__.'/../Logs/runtime.log';
+
+    public static $foreground_colors = array();
+    public static $background_colors = array();
+
+    public static function info($msg)
+    {
+        self::add($msg,'info');
+    }
+
+    public static function error($msg)
+    {
+        self::add($msg,'error');
+    }
+
+    public static function warning($msg)
+    {
+        self::add($msg,'warning');
+    }
+
+    public static function debug($msg)
+    {
+        self::add($msg,'debug');
+    }
+
+    public static function add($msg,$log_file_path,$log_type)
+    {
+        if(!empty($log_type))
+        {
+            $log_type = strtoupper($log_type);
+
+            $msg = sprintf("%s [%s] %s".PHP_EOL,date('Y-m-d H:i:s',time()),$log_type,$msg);
+
+            file_put_contents(self::$log_file_path,$msg, FILE_APPEND | LOCK_EX);
+        }
+    }
+    public static function getForegroundColors()
+    {
+        return array_keys(self::$foreground_colors);
+    }
+
+    public static function getBackgroundColors()
+    {
+        return array_keys(self::$background_colors);
+    }
+
+    public static function _initBackgroundColors()
+    {
+        self::$foreground_colors = array(
+            'black'     =>'0;30', 'dark_gray'   =>'1;30',
+            'blue'      =>'0;34', 'light_blue'  =>'1;34',
+            'green'     =>'0;32', 'light_green' =>'1;32',
+            'cyan'      =>'0;36', 'light_cyan'  =>'1;36',
+            'red'       =>'0;31', 'light_red'   =>'1;31',
+            'purple'    =>'0;35', 'light_purple'=>'1;35',
+            'brown'     =>'0;33', 'yellow'      =>'1;33',
+            'light_gray'=>'0;37', 'white'       =>'1;37',
+        );
+        self::$background_colors = array(
+            'red'=>'41', 'green'    =>'42',
+            'yellow'=>'43', 'blue'  =>'44',
+            'magenta'=>'45', 'cyan' =>'46',
+            'light_gray'=>'46',
+        );
+    }
+
+    public static function notice($string,$type = 'info',$background_color=null)
+    {
+
+        self::_initBackgroundColors();
+
+        switch ($type)
+        {
+            case 'info':
+                $foreground_color = 'green';
+                break;
+            case 'error':
+                $foreground_color = 'red';
+                break;
+            case 'warning':
+                $foreground_color = 'brown';
+                break;
+            case null:
+                $foreground_color = 'green';
+                break;
+            default:
+                $foreground_color = $type;
+        }
+
+        $colored_string = "";
+
+        if (isset(self::$foreground_colors[$foreground_color]))
+        {
+            $colored_string .= "\033[" . self::$foreground_colors[$foreground_color] . "m";
+        }
+        if (isset(self::$background_colors[$background_color]))
+        {
+            $colored_string .= "\033[" . self::$background_colors[$background_color] . "m";
+        }
+
+        $colored_string .=  $string . "\033[0m";
+
+        echo sprintf(self::getLocalTime().' [%s] '.$colored_string.PHP_EOL,strtoupper($type));
+    }
+
+    public static function getLocalTime()
+    {
+        return date('Y-m-d H:i:s',time());
+    }
+
+    public static function clear_command_line()
+    {
+        $line = array(27, 91, 72, 27, 91, 50, 74);
+        foreach ($line as $ascii)
+        {
+            echo chr($ascii);
+        }
+    }
+    public static function replace_echo($message,$force_clear_lines = NULL)
+    {
+        static $last_lines = 0;
+        if(!is_null($force_clear_lines))
+        {
+            $last_lines = $force_clear_lines;
+        }
+        // 获取终端宽度
+        $toss = $status = null;
+        $term_width = exec('tput cols', $toss, $status);
+        if($status || empty($term_width))
+        {
+            $term_width = 64;
+        }
+        $line_count = 0;
+        //循环清除当前行数,用\n分割
+        foreach(explode("\n", $message) as $line)
+        {
+            $line_count += count(str_split($line, $term_width));
+        }
+        for($i = 0; $i < $last_lines; $i++)
+        {
+            echo "\r";
+            echo "\033[K";
+            echo "\033[1A";
+            echo "\r";
+            echo "\033[K";
+            echo "\r";
+        }
+        $last_lines = $line_count;
+        echo $message."\n";
+    }
+
+
+}
--- services.php
+++ services.php
@@ -10,51 +10,105 @@ use Workerman\Worker;
 use \Workerman\Lib\Timer;
 use \Workerman\Lib\Requests;
 use \Workerman\Lib\XiaoeSdk;
-
+use \Workerman\Lib\logger;
 
 require_once __DIR__ . '/Autoloader.php';
 
+header("content-Type: text/html; charset=Utf-8");
+
+date_default_timezone_set('PRC');
+
 class Mail
 {
-    private static $time_interval = 1;
     private static $timer_id;
-    private static $version  = '2.0';
-    private static $use_type = '0';
+    private static $config;
 
-
-    public function check_orders($to, $content)
+    public function _check_order()
     {
         //获取订单列表
-        $cmd    = "order.list.get";
-        $params = array();
+        $cmd      = static::$config['order']['cmd'];
+        $use_type = static::$config['order']['use_type'];
+        $version  = static::$config['order']['version'];
 
-        $result = XiaoeSdk::send($cmd,$params,self::$use_type,self::$version);
+        $params = array(
+            'page_size' => static::$config['order']['page_size']
+        );
+
+        $result = XiaoeSdk::send($cmd,$params,$use_type,$version);
 
         if($result['code'] == 0 && $result['msg'] == 'success')
         {
 
-            print_r($result);
-//            foreach ($result['data'] as $value)
-//            {
-//
-//                //已支付订单,触发邮件、短信提醒
-//                if($value['order_state'] == 1)
-//                {
-//                    $created_at = strtotime($value['created_at']);
-//
-//                    //30秒之内付款的发送邮件
-//                    if((time() - $created_at) < 30)
-//                    {
-//
-//                    }
-//                }
-//            }
+            foreach ($result['data'] as $value)
+            {
+                //已支付订单,触发邮件、短信提醒
+                if($value['order_state'] == 1)
+                {
+                    $created_at = strtotime($value['created_at']);
+
+                    //30秒之内的订单发送邮件,定时器每三十秒拿一次订单数据
+                    if((time() - $created_at) < 30)
+                    {
+                        $con = sprintf('监测新订单:%s,user_id:%s,created_at:%s',$value['order_id'],$value['user_id'],$value['created_at']);
+                        logger::add($con,'news_order_success.log','info');
+                        static::send_email($value['user_id'],$value['title']);
+                    }
+                    else
+                    {
+                        continue;
+                        //echo "暂时没有新订单~".PHP_EOL;
+                    }
+                }
+            }
         }
     }
 
-    public function sendLater($to, $content)
+    public function send_email($user_id = 1,$title)
     {
-        self::$timer_id = Timer::add(self::$time_interval, array($this, 'check_orders'), array($to, $content), true);
+
+        $email_user_config = static::$config['email']['focusUser'];
+
+        $SoapClient = new SoapClient(static::$config['soap_client'],array('trace' => 1));
+
+        $FocusUser = array(
+            'Email'     => $email_user_config['Email'],
+            'Password'  => $email_user_config['Password'],
+        );
+
+
+        $FocusEmail = array(
+            'Body'=>'Halo baby,This is demo verison!'.date('Y-m-d H:i:s',time()),
+            'IsBodyHtml'=> $email_user_config['IsBodyHtml']
+        );
+
+
+        $email_task_config = static::$config['email']['focusTask'];
+
+        $FocusTask = array(
+            'TaskName'    => $email_task_config['TaskName'],
+            'SenderEmail' => $email_task_config['SenderEmail'],
+            'ReplyName'   => "Reply",
+            'ReplyEmail'  => $email_task_config['ReplyEmail'],
+            'SendDate'    => date('Y-m-d\TH:m:s',time()),
+        );
+
+        $subject="【幕后圈课堂】感谢您购买{$title}混音系列课程。{$FocusTask['SendDate']}";
+
+        $FocusReceiver  = array(
+            'Email'=>'465360967@qq.com',
+        );
+
+
+        //send one email
+        $result= $SoapClient->SendOne(array("user"=>$FocusUser,"email"=>$FocusEmail,"subject"=>$subject,"receiver"=>$FocusReceiver));
+
+        print_r($result) ;
+    }
+
+    public function _init()
+    {
+        static::$config = include_once "Lib/Config.php";
+        static::$timer_id = Timer::add(3, array($this, '_check_order'), array(), true);
     }
 }
 
@@ -66,9 +120,7 @@ $task->onWorkerStart = function($task)
     if($task->id === 0)
     {
         $mail = new Mail();
-        $to = 'workerman@workerman.net';
-        $content = 'hello workerman';
-        $mail->sendLater($to, $content);
+        $mail->_init();
     }
 
 };
